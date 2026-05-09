a0ddc1a97756b29d3bc91055cb4b82fb84c030ae
Jefith Shalin <jefithshalin@gmail.com>
2026-05-09 21:40:04 +0530

    Create sticky_notes_html_region.sql

---
+++ sticky_notes_html_region.sql
@@ -0,0 +1,14 @@
+<div id="note-toolbar">
+  <span class="toolbar-label">
+    <strong>Click Any Color to Add Sticky Note</strong>
+  </span>
+  <div class="color-btns">
+    <button class="btn-color" data-color="yellow" title="Yellow" style="background:#fff475;"></button>
+    <button class="btn-color" data-color="pink"   title="Pink"   style="background:#f28b82;"></button>
+    <button class="btn-color" data-color="blue"   title="Blue"   style="background:#cbf0f8;"></button>
+    <button class="btn-color" data-color="green"  title="Green"  style="background:#ccff90;"></button>
+    <button class="btn-color" data-color="purple" title="Purple" style="background:#d7aefb;"></button>
+    <button class="btn-color" data-color="orange" title="Orange" style="background:#fbbc04;"></button>
+  </div>
+</div>
+<div id="notes-board"></div>
